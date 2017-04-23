a65a2fab587eeed41d80f228b5e07c0b59df3c96
ArsenArsen <ArsenArsen@users.noreply.github.com>
2017-04-23 20:44:48 +0200

    OOps

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     a.setApplicationName("KShare");
     a.setOrganizationName("ArsenArsen");
-    a.setApplicationName("1.0");
+    a.setApplicationVersion("1.0");
     MainWindow w;
     w.show();
     return a.exec();
