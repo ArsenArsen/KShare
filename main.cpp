a4d17949dc0167e190a9211284ab4d8e66082ef2
ArsenArsen <ArsenArsen@users.noreply.github.com>
2017-05-13 18:54:13 +0200

    fuck

--- main.cpp
+++ main.cpp
@@ -33,7 +33,7 @@ int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     a.setApplicationName("KShare");
     a.setOrganizationName("ArsenArsen");
-    a.setApplicationVersion("1.1");
+    a.setApplicationVersion("2.1");
 
     QCommandLineParser parser;
     parser.addHelpOption();
