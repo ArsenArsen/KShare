b6931e13c5f610c6eb8482aae820e88870bd7853
ArsenArsen <ArsenArsen@users.noreply.github.com>
2017-05-13 23:49:16 +0200

    Bookmars are so 2015

--- platformspecifics/mac/macbackend.cpp
+++ platformspecifics/mac/macbackend.cpp
@@ -3,4 +3,7 @@
 QPixmap PlatformBackend::getCursor() {
 #warning "TODO: Mac backend"
     return std::tuple<QPoint, QPixmap>(QPoint(0, 0), QPixmap());
+    // Monday: https://developer.apple.com/reference/appkit/nscursor/1527062-image
+    // Some on how to do NSImage -> QPixmap: http://stackoverflow.com/a/2468961/3809164
+    // This is gonna be easier than with Windows
 }
