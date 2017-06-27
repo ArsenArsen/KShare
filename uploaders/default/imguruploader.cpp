cb45fff054dc06f683dd25dd5393a646d4505472
ArsenArsen <ArsenArsen@users.noreply.github.com>
2017-06-27 23:44:31 +0200

    I hoped this fixes video uploading but nope

--- uploaders/default/imguruploader.cpp
+++ uploaders/default/imguruploader.cpp
@@ -3,18 +3,24 @@
 #include <QBuffer>
 #include <QJsonObject>
 #include <QJsonValue>
+#include <formats.hpp>
 #include <io/ioutils.hpp>
 #include <notifications.hpp>
 #include <screenshotutil.hpp>
 
-void ImgurUploader::doUpload(QByteArray byteArray, QString) {
+void ImgurUploader::doUpload(QByteArray byteArray, QString format) {
     if (byteArray.size() > 1e+7) {
         notifications::notify("KShare imgur Uploader ", "Failed upload! Image too big");
         return;
     }
+    QString mime;
+    if (formats::normalFormatFromName(format) != formats::Normal::None)
+        mime = formats::normalFormatMIME(formats::normalFormatFromName(format));
+    else
+        mime = formats::recordingFormatMIME(formats::recordingFormatFromName(format));
     ioutils::postJson(QUrl("https://api.imgur.com/3/image"),
                       QList<QPair<QString, QString>>()
-                      << QPair<QString, QString>("Content-Type", "application/x-www-form-urlencoded")
+                      << QPair<QString, QString>("Content-Type", mime.toLatin1())
                       << QPair<QString, QString>("Authorization", "Client-ID 8a98f183fc895da"),
                       byteArray, [](QJsonDocument res, QByteArray, QNetworkReply *) {
                           QString result = res.object()["data"].toObject()["link"].toString();
